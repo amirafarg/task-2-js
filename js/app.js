4e2189549f25ffa65de7e10b967e527bb2c16b2a
amirafarag <amiraazam301@gmail.com>
2024-03-13 21:59:34 +0200

    task-2

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// for storage this number -->array
+
+var studentScours=[20,30,50];
+console.log(studentScours);
+
+// appare-number-2
+console.log(studentScours[2]);
+
+// for add number to specifec Number
+
+// studentScours[0]= studentScours[0]+[3];
+// console.log(studentScours[0]= studentScours[0]+[3] );
+
+{/* <loap> to add numbers to all scours */}
+
+var index= 0;
+while (index <=4) {
+    console.log(studentScours[index]);
+    studentScours[index]=studentScours[index] +4;
+    index=index+1;
+}
